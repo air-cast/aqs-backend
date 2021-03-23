5face77a4724cfd37ceaf18db03133c8a87dbd2b
farooq-maker <>
2021-03-24 02:24:00 +0500

    AIRCAST :: Getters/Setters added as they are used to map json of request to class objects(models) | Timestamp replaced with LocalDateTime to fix autogeneration bug

--- src/main/java/com/aircast/aqsrestapi/datarecord/DataRecord.java
+++ src/main/java/com/aircast/aqsrestapi/datarecord/DataRecord.java
@@ -1,6 +1,9 @@
 package com.aircast.aqsrestapi.datarecord;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "datarecord")
@@ -9,6 +12,10 @@ public class DataRecord {
     @Id
     @GeneratedValue
     Long id;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 
     @Column(nullable = false)
     Long deviceId;
@@ -23,4 +30,51 @@ public class DataRecord {
     Long aqi;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(Long deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public Long getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Long temperature) {
+        this.temperature = temperature;
+    }
+
+    public Long getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Long humidity) {
+        this.humidity = humidity;
+    }
+
+    public Long getAqi() {
+        return aqi;
+    }
+
+    public void setAqi(Long aqi) {
+        this.aqi = aqi;
+    }
 }
--- src/main/java/com/aircast/aqsrestapi/device/Device.java
+++ src/main/java/com/aircast/aqsrestapi/device/Device.java
@@ -1,10 +1,12 @@
 package com.aircast.aqsrestapi.device;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.security.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "device")
@@ -13,10 +15,34 @@ public class Device {
     @Id
     Long id;
 
-    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    Timestamp createdAt;
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 
     @Column(nullable = false)
     Long codeVersionId;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getCodeVersionId() {
+        return codeVersionId;
+    }
+
+    public void setCodeVersionId(Long codeVersionId) {
+        this.codeVersionId = codeVersionId;
+    }
 }

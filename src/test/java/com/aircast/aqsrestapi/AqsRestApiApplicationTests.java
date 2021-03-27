2a2df44fd0e2ee33465e7c9f2caa6c6bdc9dcfeb
unknown <farooq.maker@gmail.com>
2021-03-27 23:45:09 +0000

    src re-added

---
+++ src/test/java/com/aircast/aqsrestapi/AqsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aircast.aqsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqsRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

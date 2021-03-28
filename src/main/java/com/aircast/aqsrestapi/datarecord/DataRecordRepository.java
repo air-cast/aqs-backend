899f5bc6ca4f6e9bbcd883cef577ca9b363b1458
unknown <farooq.maker@gmail.com>
2021-03-28 00:58:56 +0000

    clean up

--- src/main/java/com/aircast/aqsrestapi/datarecord/DataRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aircast.aqsrestapi.datarecord;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataRecordRepository extends JpaRepository<DataRecord, Long> {
-
-}
--- src/main/java/com/aircast/aqsrestapi/device/DeviceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aircast.aqsrestapi.device;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {
-
-    public Device findDeviceById(Long deviceid);
-
-}
